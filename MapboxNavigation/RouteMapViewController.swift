b99282bcedffe10e77ab97a58ecd550e5d1c6fba
Shan Ma <shanma@usc.edu>
2020-10-22 18:08:15 -0700

    add the constraint to height of bottom banner and the attributionButton, to fix the over view mode of navigation map

--- MapboxNavigation/RouteMapViewController.swift
+++ MapboxNavigation/RouteMapViewController.swift
@@ -281,7 +281,6 @@ class RouteMapViewController: UIViewController {
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         mapView.enableFrameByFrameCourseViewTracking(for: 3)
-        navigationView.reinstallRequiredConstraints()
     }
 
     override func viewDidLayoutSubviews() {
